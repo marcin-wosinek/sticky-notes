af523cc81fc99ba8703591b5731c392ffeea9650
Marcin Wosinek <marcin.wosinek@gmail.com>
2014-12-03 23:48:08 +0000

    Put default values for position

--- app/scripts/controllers/add.js
+++ app/scripts/controllers/add.js
@@ -9,13 +9,24 @@
  */
 angular.module('stickyNotesApp')
   .controller('AddCtrl', function ($scope, $location, notesStorage) {
+    var emptyNote = {
+      position: {
+        x: 0,
+        y: 0
+      },
+      title: '',
+      description: ''
+    };
+
+    $scope.note = angular.copy(emptyNote);
+
     $scope.add = function (note) {
       notesStorage.add(note);
       $location.path('/notes');
     };
 
     $scope.reset = function (note) {
-      angular.copy({}, note);
+      angular.copy(emptyNote, note);
     };
 
     $scope.cancel = function () {
