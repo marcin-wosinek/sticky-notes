008a3368be2c456f21054c0a6b4bf1f044270609
Marcin Wosinek <marcin.wosinek@gmail.com>
2014-12-14 10:54:58 +0000

    #15 improve nsDimensions documentation

--- app/scripts/directives/sn-dimensions.js
+++ app/scripts/directives/sn-dimensions.js
@@ -5,14 +5,21 @@
  * @name stickyNotesApp.directive:snDimensions
  * @description
  * # snDimensions
+ *
+ * Directive that reads element width and height and expose it to the model. Keeping model up to
+ * date with actual size is a chalenge because js lacks event for resizing the element. We check
+ * the size at 4 different moments.
  */
 angular.module('stickyNotesApp')
   .directive('snDimensions', function ($window, $parse, $timeout) {
     return {
       restrict: 'A',
       link: function postLink(scope, element, attrs) {
+
+        // We parse value set to sn-dimensions attribute
         var parsedSnDimensions = $parse(attrs.snDimensions);
 
+        // function that update scope with expected values
         function setDimensions() {
           parsedSnDimensions.assign(scope, {
             x: element[0].offsetWidth,
@@ -20,16 +27,21 @@ angular.module('stickyNotesApp')
           });
         }
 
-        // read link-time size
+        // We read element size *before compiling & binding* elements inslide it. Due to angular
+        // architecture we don't have posibility to wait for the internal element - directive
+        // priorities are compared *at the same element*.
         setDimensions();
 
-        // force reading after digest cycle
+        // Timeout with 0 will run function *after current* digest cycle - ie when all values
+        // are bind to their location.
         $timeout(setDimensions, 0);
 
+        // When window is resized, we check the size again
         $window.onresize = function() {
           scope.$apply(setDimensions);
         };
 
+        // We allow controller to comunicate that it have done something that can affect the size.
         scope.$on('snResize', setDimensions);
       }
     };
