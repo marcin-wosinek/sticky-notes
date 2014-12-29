9dbfea0c19d4353dcb930f8a62d800a4a6afb8da
Grzegorz <grma@grapeup.com>
2014-12-29 19:38:39 +0100

    clean board

--- app/scripts/controllers/settings.js
+++ app/scripts/controllers/settings.js
@@ -8,9 +8,13 @@
  * Controller of the stickyNotesApp
  */
 angular.module('stickyNotesApp')
-  .controller('SettingsCtrl', function ($scope, $mdBottomSheet, $mdDialog) {
-    $scope.cleanBoard = function () {
-      // TODO implement cleaning board
+  .controller('SettingsCtrl', function ($scope, $mdBottomSheet, $mdDialog,notesStorage) {
+    $scope.archiveAll = function () {
+      var notes = notesStorage.getAll();
+      var length = notes.length;
+      for(var i=length-1;i>=0;i--){
+        notesStorage.archive(notes[i])
+      }
     };
 
     $scope.exitSettings = function () {
