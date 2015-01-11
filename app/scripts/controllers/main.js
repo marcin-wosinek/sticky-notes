4d792ae06adc38394eb1c2f210cc3d37c69b4f07
Grzegorz Marzencki <gm@pureepic.eu>
2015-01-11 21:30:11 +0100

    authors section improved general fixes

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,17 @@
  * Controller of the stickyNotesApp
  */
 angular.module('stickyNotesApp')
-  .controller('MainCtrl', function ($scope, $route, $timeout, dimensionDelay) {
+  .controller('MainCtrl', function ($scope, $route, $timeout, $location, dimensionDelay) {
+    $scope.location = function(args){
+        var locationArray = $location.$$path.split("/"),
+            location = locationArray[locationArray.length-1];
+        if(typeof args === 'object'){
+            return args.indexOf(location) !== -1;
+        }
+        else{
+            return args === location;
+        }
+    };
     var setRoute = function () {
       $scope.route = $route.current.loadedTemplateUrl.match('\/(.*)\.html')[1];
       $timeout(function () {
