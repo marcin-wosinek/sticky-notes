faa1ed0fd7576786540a6571798fe9fe78ac9f47
Marcin Wosinek <marcin.wosinek@gmail.com>
2015-02-21 10:04:15 +0000

    fix error in data filter
    init #47

--- app/scripts/filters/yesterday.js
+++ app/scripts/filters/yesterday.js
@@ -4,9 +4,12 @@ angular.module('stickyNotesApp').filter('yesterday', function () {
   return function (input) {
     var yesterday = new Date(),
       output = [],
-      date =  yesterday.getFullYear() + '-' + yesterday.getMonth() + '-' + yesterday.getDate();
+      date = '';
 
     yesterday.setDate(yesterday.getDate() - 1);
+
+    date =  yesterday.getFullYear() + '-' + yesterday.getMonth() + '-' + yesterday.getDate();
+
     input.forEach(function (item) {
       if (item.archivedDate.fullDate === date) {
         output.push(item);
