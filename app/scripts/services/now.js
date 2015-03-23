4494496eb815b8138d15c5a43cac44e3c19f3bec
Marcin Wosinek <marcin.wosinek@gmail.com>
2015-03-23 16:04:19 +0000

    fix travis builds

--- app/scripts/services/now.js
+++ app/scripts/services/now.js
@@ -7,4 +7,6 @@
  * Constant in the stickyNotesApp, that keeps now as a reference
  */
 angular.module('stickyNotesApp')
-  .constant('now', new Date());
+  .factory('now', function() {
+    return new Date();
+  });
